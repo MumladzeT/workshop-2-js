13fd970f43d1615a7d1434e5fa909ef3070c69c2
MumladzeT <tatiamumladze2004@gmail.com>
2024-04-08 00:24:25 +0400

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,68 @@
+
+
+
+
+// let array1= [1, 2, 4, -1, -3, 8, 7, 0, -5, 18, 12];
+
+// console.log(array1 [1];[2];[5];[6]);
+
+
+// let array2 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// for(let i=0; i<5; i=i+1; ) {
+//     console.log("არის");
+// }
+
+
+let array3 = [1, 2, 3, 4, 5];
+
+console.log(array3[0] + array3[1] + array3[2] + array3[3] + array3[4]);
+
+
+let array4 = [1, 2, 3, 7, 6, 9];
+
+console.log(array4[0] + array4[1] + array4[2] + array4[3] + array4[4] + array4[5] / 6);
+
+
+// let array5 = [1, 2, 3, 7, 6, 9];
+
+// console.log(array5[0];[1];[2];[4];[5]);
+
+
+
+let user = {
+    firstname: "giorgi",
+    lastname: "smith",
+    age: 25,
+    studentstatus: "active"
+};
+
+console.log(user.studentstatus);
+
+
+// let user = {
+//     name: 'giorgi',
+//     age: 20,
+//     studentstatus: 'active'}
+
+// if(user.age < 18)
+//     console.log("hello");
+
+
+
+
+if (user.firstname = "levani") {
+    console.log("hello levani");
+}
+
+
+// if (user.studentstatus = "active";) {
+//     // if (user.age < 25) {
+//         console.log("hello world");
+//     }
+
+// }
+// else {
+//     console.log("error");
+// }
+
+
